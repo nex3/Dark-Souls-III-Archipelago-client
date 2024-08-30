336f4ba47be0f76e08122d2d28a9780dc35e547a
Natalie Weizenbaum <nex342@gmail.com>
2024-08-29 23:31:21 -0700

    Release 3.1.0-alpha.10

--- archipelago-client/Core.h
+++ archipelago-client/Core.h
@@ -40,7 +40,7 @@
 #define FE_ApplySettings 14
 #define FE_PatternFailed 15
 
-#define VERSION "3.1.0-alpha.9"
+#define VERSION "3.1.0-alpha.10"
 
 
 class CCore: public modengine::ModEngineExtension {
